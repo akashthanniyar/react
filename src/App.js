0842d6f77a670efcdcd4f267f6806656b4a5ffed
akashthanniyar <akashremesh@gmail.com>
2023-04-06 12:04:45 +0530

    passingData

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
-}
+  const expense = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      LocationOfExpenditure: "Bangalore"
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), LocationOfExpenditure: "Chennai" },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      LocationOfExpenditure: "Bangalore"
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),   
+      LocationOfExpenditure: "Chennai" 
+    },
+  ];
+    return (
+      <div>
+        <h2>Let's get started!</h2>
+        {expense.map((expense)=>{
+          return (
+            <ExpenseItem 
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date}
+            LocationOfExpenditure={expense.LocationOfExpenditure}
+            ></ExpenseItem>
+          )
+        })}
+      </div>
+    );
+  }
+    
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
-function ExpenseItem() {
+import "./ExpenseItem.css"
+
+function ExpenseItem(props) {
+
   return (
-    <div>
-      <div>
-        <h2>Expense item!</h2>
+       <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2> 
+        <h2>{props.LocationOfExpenditure}</h2>
+      <div className="expense-item__price">${props.amount}</div>
       </div>
-      <div>Food Rs 10</div>
-      <div>Petrol Rs 100</div>
-      <div>Movies Rs 200</div>
     </div>
   );
 }
